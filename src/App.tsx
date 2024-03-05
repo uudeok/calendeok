e1fdd99abb38e5ca689255aad172f8c05eb58db5
YuDeok <awesome36@daum.net>
2024-03-05 22:38:42 +0900

    App.tsx 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { Time } from "./@types";
 import Calender from "./Calendeok";
 import dayjs from "dayjs";
+import ConditionalDisplay from "./common/ConditionalDisplay";
 
 const SUNDAY = 0;
 
 const App = () => {
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDate = (date: Date) => {
     console.log("date", date);
@@ -36,29 +38,46 @@ const App = () => {
 
   const dayInEnglish = ["S", "M", "T", "W", "T", "F", "S"];
 
+  const pickDate = dayjs(date).format("YYYY-MM-DD");
+
+  const handleCalender = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <Calender
-      selected={date}
-      onClick={handleDate}
-      curMonthOnly={false}
-      minDate={new Date()}
-      maxDate={laterOneMonth}
-      filterDate={isOpenDay}
-      showTimePicker={true}
-      onClickTime={handleTime}
-      selectedTime={time}
-      timeInterval={30}
-      minTime="09:00"
-      maxTime="21:00"
-      filterTime={filterTime}
-      placeholder="시간 선택"
-      excludeTimes={[
-        new Date(date.setHours(12, 0, 0)),
-        new Date(date.setHours(12, 30, 0)),
-        new Date(date.setHours(13, 0, 0)),
-        new Date(2024, 2, 9, 15, 0, 0),
-      ]}
-    />
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-3xl text-orange-600 mb-4 mt-4">Calendeok</h2>
+      <input
+        className="border border-black p-2 text-center mb-4"
+        value={`${pickDate} ${time}`}
+        onClick={handleCalender}
+        readOnly
+      />
+      <ConditionalDisplay condition={isOpen}>
+        <Calender
+          selected={date}
+          onClick={handleDate}
+          curMonthOnly={false}
+          minDate={new Date()}
+          maxDate={laterOneMonth}
+          filterDate={isOpenDay}
+          showTimePicker={true}
+          onClickTime={handleTime}
+          selectedTime={time}
+          timeInterval={30}
+          minTime="09:00"
+          maxTime="21:00"
+          filterTime={filterTime}
+          placeholder="시간 선택"
+          excludeTimes={[
+            new Date(date.setHours(12, 0, 0)),
+            new Date(date.setHours(12, 30, 0)),
+            new Date(date.setHours(13, 0, 0)),
+            new Date(2024, 2, 9, 15, 0, 0),
+          ]}
+        />
+      </ConditionalDisplay>
+    </div>
   );
 };
 export default App;
--- src/Calendeok/index.tsx
+++ src/Calendeok/index.tsx
@@ -55,7 +55,7 @@ const Calender = ({
   const [curMonth, setCurMonth] = useState(selected.getMonth());
 
   return (
-    <div className="flex flex-col w-96 m-auto rounded-md border mt-10">
+    <div className="flex flex-col w-96 rounded-md border">
       <MonthController
         setCurMonth={setCurMonth}
         setCurYear={SetCurYear}
