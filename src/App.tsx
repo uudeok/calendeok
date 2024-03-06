d241ddbb070d3f6994e1445437421f3d7669a4ca
YuDeok <awesome36@daum.net>
2024-03-06 11:59:52 +0900

    css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Time } from "./@types";
 import Calender from "./Calendeok";
 import dayjs from "dayjs";
@@ -10,15 +10,13 @@ const App = () => {
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-
+  const calendarRef = useRef<HTMLDivElement>(null);
   const handleDate = (date: Date) => {
-    console.log("date", date);
     setDate(date);
     setTime("");
   };
 
   const handleTime = (time: string) => {
-    console.log("time", time);
     setTime(time);
   };
 
@@ -36,46 +34,59 @@ const App = () => {
     return current < selected;
   };
 
-  const dayInEnglish = ["S", "M", "T", "W", "T", "F", "S"];
-
   const pickDate = dayjs(date).format("YYYY-MM-DD");
 
-  const handleCalender = () => {
+  const handleCalendarClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    // 달력 영역 외의 영역 클릭인지 확인
+    if (
+      calendarRef.current &&
+      !calendarRef.current.contains(e.target as Node)
+    ) {
+      setIsOpen(false); // isOpen 상태 변경
+    }
+  };
+
+  const handleCalendarToggle = () => {
     setIsOpen((prev) => !prev);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full bg-blue-100">
+    <div
+      className="flex flex-col items-center w-full h-full bg-blue-100"
+      onClick={handleCalendarClick}
+    >
       <h2 className="text-3xl text-orange-600 mb-4 mt-4">Calendeok</h2>
       <input
         className="border border-black p-2 text-center mb-4"
         value={`${pickDate} ${time}`}
-        onClick={handleCalender}
+        onClick={handleCalendarToggle}
         readOnly
       />
       <ConditionalDisplay condition={isOpen}>
-        <Calender
-          selected={date}
-          onClick={handleDate}
-          curMonthOnly={false}
-          minDate={new Date()}
-          maxDate={laterOneMonth}
-          filterDate={isOpenDay}
-          showTimePicker={true}
-          onClickTime={handleTime}
-          selectedTime={time}
-          timeInterval={30}
-          minTime="09:00"
-          maxTime="21:00"
-          filterTime={filterTime}
-          placeholder="시간 선택"
-          excludeTimes={[
-            new Date(date.setHours(12, 0, 0)),
-            new Date(date.setHours(12, 30, 0)),
-            new Date(date.setHours(13, 0, 0)),
-            new Date(2024, 2, 9, 15, 0, 0),
-          ]}
-        />
+        <div ref={calendarRef}>
+          <Calender
+            selected={date}
+            onClick={handleDate}
+            curMonthOnly={false}
+            minDate={new Date()}
+            maxDate={laterOneMonth}
+            filterDate={isOpenDay}
+            showTimePicker={true}
+            onClickTime={handleTime}
+            selectedTime={time}
+            timeInterval={30}
+            minTime="09:00"
+            maxTime="21:00"
+            filterTime={filterTime}
+            placeholder="시간 선택"
+            excludeTimes={[
+              new Date(date.setHours(12, 0, 0)),
+              new Date(date.setHours(12, 30, 0)),
+              new Date(date.setHours(13, 0, 0)),
+              new Date(2024, 2, 9, 15, 0, 0),
+            ]}
+          />
+        </div>
       </ConditionalDisplay>
     </div>
   );
