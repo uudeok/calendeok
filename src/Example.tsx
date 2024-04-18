da43bd7e0cf0abfbfa0ead5a3b6634d51df44736
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-04-18 19:11:28 +0900

    Example useCalendar 로 수정

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,19 +1,68 @@
-import Calender from "./Calendeok";
-import { useState } from "react";
+import { useState } from 'react';
+import useCalendar from './hooks/useCalendar';
+import dayjs from 'dayjs';
 
 const Example = () => {
-  const [date, setDate] = useState(new Date());
+    const [selected, setSelected] = useState<Date>(new Date());
+    const selectedDate = dayjs(selected).format('YYYY-MM-DD');
 
-  const handleDate = (date: Date) => {
-    setDate(date);
-  };
+    const handleDate = (date: Date) => {
+        setSelected(date);
+    };
 
-  return (
-    <>
-      <h2 className="text-3xl text-orange-600 mb-4 mt-4 font-pre">Calendeok</h2>
-      <Calender onClick={handleDate} selected={date} />
-    </>
-  );
+    const { prevController, nextController, body, curMonth, curYear, weeks } = useCalendar();
+
+    return (
+        <>
+            <div className="font-bold w-40 text-center border border-black p-2 mt-4 mb-2">{selectedDate}</div>
+            <div className="mx-auto flex flex-col items-center text-center w-350 h-450 bg-white p-4">
+                <div className="p-4 text-2xl font-bold text-left ">
+                    <div>
+                        {curYear}년 {curMonth + 1}월
+                    </div>
+                </div>
+
+                <div className="flex justify-end gap-4">
+                    <button onClick={prevController} className="border-none bg-transparent cursor-pointer">
+                        이전 달
+                    </button>
+                    <button onClick={nextController} className="border-none bg-transparent cursor-pointer">
+                        다음 달
+                    </button>
+                </div>
+                <table className="p-2 mt-4 w-full">
+                    <thead>
+                        <tr>
+                            {weeks.ko.map((week, index) => (
+                                <th key={index}>{week}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {body.map((rows, index) => (
+                            <tr key={index}>
+                                {rows.map((row) => (
+                                    <td key={row.value} className={`text-15 relative pb-2.5 pt-2 text-center`}>
+                                        <button
+                                            className={`border-none cursor-pointer w-8 h-8  ${
+                                                selected === row.date && 'bg-blue-500 text-white rounded-full'
+                                            } hover:bg-blue-500 hover:text-white hover:rounded-full`}
+                                            onClick={() => handleDate(row.date)}
+                                        >
+                                            {row.date.getDate()}
+                                        </button>
+                                        {dayjs().isSame(row.date, 'day') && (
+                                            <div className="absolute w-full text-ss text-center">Today</div>
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
 
 export default Example;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "bright-blue": "#1773EF",
-        "light-gray": "#D6E1E5",
-        "light-green": "#19C37D",
-      },
-      fontSize: {
-        sm: "0.7rem",
-      },
-      width: {
-        14: "14.28%",
-      },
-      fontFamily: {
-        pre: ["Pretendard-Regular"],
-      },
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            height: {
+                450: '450px',
+            },
+            colors: {
+                'bright-blue': '#1773EF',
+                'light-gray': '#D6E1E5',
+                'light-green': '#19C37D',
+            },
+            fontSize: {
+                ss: '10px',
+                sm: '0.7rem',
+                15: '15px',
+            },
+            width: {
+                14: '14.28%',
+                350: '350px',
+            },
+            fontFamily: {
+                pre: ['Pretendard-Regular'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
